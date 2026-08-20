c8d22e0021c29d3d93c3b884d6a01333b32ce5c4
lifesalt5-netizen <lifesalt5@gmail.com>
2026-08-20 13:37:11 -0400

    Route capture discovery through canonical MILES connector

--- CONNECTORS/MILES/connector.js
+++ CONNECTORS/MILES/connector.js
@@ -17,6 +17,7 @@ const {
 
 const HANDLER_PATHS = Object.freeze({
   BUSINESS_EXECUTION: "../../SERVICES/BusinessExecutionEngineServiceV2",
+  CAPTURE_CAPACITY_DISCOVERY: "../../SERVICES/revenue/CaptureCapacityAutonomousExecutionService",
   PROVIDER_AUTHORITY: "../../SERVICES/ProviderAuthorityRegistryService",
   PROVIDER_AUTHORITY_REGISTRY: "../../SERVICES/ProviderAuthorityRegistryService",
   PROVIDER_SYNC: "../../SERVICES/ProviderSynchronizationService",
