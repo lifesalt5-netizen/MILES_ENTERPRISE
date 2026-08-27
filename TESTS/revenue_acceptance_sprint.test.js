a6241c953421a101b90f219eed7e09cb9a4a8c10
lifesalt5-netizen <lifesalt5@gmail.com>
2026-08-26 21:49:08 -0400

    Avoid overlap with placement completeness PR

--- TESTS/revenue_acceptance_sprint.test.js
+++ TESTS/revenue_acceptance_sprint.test.js
@@ -21,6 +21,4 @@ assert(src.includes('startsSoak: false'));
 assert(!src.includes('CreateControlledInstantlyInboxPlacementTest.js'), 'Sprint must not create another test');
 assert(!src.includes('RemediateNamecheapDmarc.js'), 'Sprint must not mutate DNS');
 
-require('./meeting_pipeline_calendly_bridge.test.js');
-
 console.log('REVENUE_ACCEPTANCE_SPRINT_SAFETY=PASS');
